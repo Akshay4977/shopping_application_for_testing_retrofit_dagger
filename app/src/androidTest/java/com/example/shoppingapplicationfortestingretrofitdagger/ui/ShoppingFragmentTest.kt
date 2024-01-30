3155a38110dd67c132524961c5c704f248c9e5e3
Akshay4977 <Djmorya@4977>
2024-01-30 17:31:52 +0530

    CI/CD first commit

--- app/src/androidTest/java/com/example/shoppingapplicationfortestingretrofitdagger/ui/ShoppingFragmentTest.kt
+++ app/src/androidTest/java/com/example/shoppingapplicationfortestingretrofitdagger/ui/ShoppingFragmentTest.kt
@@ -1,24 +1,21 @@
 package com.example.shoppingapplicationfortestingretrofitdagger.ui
 
-import android.util.Log
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
 import com.example.shoppingapplicationfortestingretrofitdagger.R
 import com.example.shoppingapplicationfortestingretrofitdagger.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.hamcrest.Matchers.allOf
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.ArgumentMatchers
-import org.mockito.Mockito.*
-import org.w3c.dom.Text
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
 
 
 @MediumTest
